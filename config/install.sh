c5439f17e6dad516179b128535f03db9070776a7
Wojciech Drabczyk <wojciechdrabczyk@icloud.com>
2025-02-17 19:31:12 +0100

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update -y
-sudo apt-get install -y stow zsh git curl tmux
+sudo apt-get install -y stow zsh git curl tmux zsh-syntax-highlighting
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ./config/configure-git.sh
