2d69ac9a13a71247168ee9deaf3c68853c84bd3f
Wojciech Drabczyk <wojciechdrabczyk@icloud.com>
2025-02-17 19:14:47 +0100

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update -y
-sudo apt-get install -y stow zsh git
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-./configure-git.sh
-./configure-ssh.sh
+sudo apt-get install -y stow zsh git curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+./config/configure-git.sh
+./config/configure-ssh.sh
 stow .
